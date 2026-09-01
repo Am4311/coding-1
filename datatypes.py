894e80d7e9fea6c468975b537e04b33092de3cfd
Am4311 <amartin843@boyslatin.net>
2026-09-01 13:37:40 +0000

    updating notes

---
+++ datatypes.py
@@ -0,0 +1,24 @@
+string - anything put in qoutes 
+characters can be letters, numbers,or symbols
+
+The hashtag is a marker that makes anything written behind it invisible to the computer 
+
+integers - whole numbers
+
+10
+20
+30
+40009909090909090909090909099009090909
+
+
+floats - numbers with decimal points
+4.0
+8.8
+5.7
+2.98768959765487656098
+
+
+booleans - true or false values
+
+true 
+false
