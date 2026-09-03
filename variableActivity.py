3dab8d3161a8c4588c998f42aad6ebae3bcfd5df
Am4311 <amartin843@boyslatin.net>
2026-09-03 13:44:44 +0000

    km \

---
+++ variableActivity.py
@@ -0,0 +1,22 @@
+Variables - a container that stores data. you can name it anything as long it descriptive 
+
+
+
+Variables Naming rules 
+1 must start with a letter (no numbers)
+2 it can have a mix between letters and numbers but no symbols 
+3 can not be a python keyword 
+
+
+Applecount = 40
+numberofstudents = 25.3 
+travelinsurance = true or false
+bronzemembershipteir = 10.01 
+goldmembership = 30.21
+silvermembership = 25.00
+applejuicecost = 5.99
+lemonnadecost = 4.79
+spritecost = 3.59
+
+camel case - when creating Variable names with muliple words every word begins with a capital letter
+snake case - using a underscore when making a Variable
