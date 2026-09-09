b63c8473f8140137f2f888246c7e9e8ce0d26fae
Am4311 <amartin843@boyslatin.net>
2026-09-09 13:45:13 +0000

    operators

--- unit 1/datatypes.py
+++ unit 1/datatypes.py
@@ -1,9 +1,9 @@
-string - anything put in qoutes 
-characters can be letters, numbers,or symbols
+#string - anything put in qoutes 
+#characters can be letters, numbers,or symbols
 
-The hashtag is a marker that makes anything written behind it invisible to the computer 
+#The hashtag is a marker that makes anything written behind it invisible to the computer 
 
-integers - whole numbers
+#integers - whole numbers
 
 10
 20
@@ -11,14 +11,14 @@ integers - whole numbers
 40009909090909090909090909099009090909
 
 
-floats - numbers with decimal points
+#floats - numbers with decimal points
 4.0
 8.8
 5.7
 2.98768959765487656098
 
 
-booleans - true or false values
+#booleans - true or false values
 
-true 
-false
+#true 
+#false
---
+++ unit 1/operators.py
@@ -0,0 +1,25 @@
+# Assingment operators - Exclusively used to 
+# asssignment values to varibales
+# think key value pairs
+
+# we use a single equal sign to reresent the assingment operator
+
+name = 'Ashton' 
+grade = 10
+school = 'true'
+
+# Arithmetic operators- used on numrical data types to preform calculations
+# intergers (whole numbers) and floats (deciman numbers) 
+
+# print is a function that lets us show code in the terminal
+
+print (8 * 9 ) # multiply operator 
+print ( 12 / 7) # divison operator 
+print ( 5 + 5 ) # addition operator 
+print ( 5 - 90 ) # subtraction operator 
+
+
+#comparison operators - set of symbols used to assess if data is the same or differnet and how they differ
+
+
+# equal signs compare is something is the same
