48f6685e9208eebe49769c89d883112f355e5d01
Am4311 <amartin843@boyslatin.net>
2026-09-10 13:39:04 +0000

    class notes

--- unit 1/operators.py
+++ unit 1/operators.py
@@ -1,5 +1,5 @@
 # Assingment operators - Exclusively used to 
-# asssignment values to varibales
+# asssignment values to varibales 
 # think key value pairs
 
 # we use a single equal sign to reresent the assingment operator
@@ -21,5 +21,35 @@ print ( 5 - 90 ) # subtraction operator
 
 #comparison operators - set of symbols used to assess if data is the same or differnet and how they differ
 
+print(10>3) #greater than
+print(10<4) #less than
+
+
+
 
 # equal signs compare is something is the same
+
+print("2" == 2)
+print (book == Book)
+
+#not equal is writtrn with !=
+#this is to check and filter for values that are not the same
+
+print(200 != 100)
+print(300!= 300)
+
+#logical operators - compares 2 conditions to check if they are true or false
+
+# and - checks if 2 conditions are true. if yes, the final result is true 
+
+print(3 > 1 and 100 > 50)
+
+# or - checks if only 1 conditions is true. if yes, the final result will be true
+
+print( 3 > 1 or 100 > 50)
+
+ #not - the opposite day operator. it will reverse the result of the logical operators.
+ 
+print(not(3 > 1 and 100 > 50)
+
+#this would be false 
