7e8e3da3470b694d2844f130988ef7a1978dcc18
Am4311 <amartin843@boyslatin.net>
2026-09-16 13:41:23 +0000

    class activity

--- unit 1/operatorsActivity.py
+++ unit 1/operatorsActivity.py
@@ -10,3 +10,23 @@ print ( "coding 1" == "Coding 1")
 
 print ("0" != 0)
 #true
+
+Book = 10.99
+Tablet = 399.99
+
+print(Book + Tablet)
+
+cart = 98.87
+AmountToGetDiscount = 100.00
+
+print(cart > AmountToGetDiscount)
+
+Shoes = 200.00
+Tax = 0.07
+
+print(Shoes * Tax )
+
+Gpa = 86 
+Recommendation = True
+
+print(Gpa > 85 and Recommendation == True)
