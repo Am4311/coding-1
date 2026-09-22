d116f36f8cfc58903da075618380628167083dd9
Am4311 <amartin843@boyslatin.net>
2026-09-22 13:42:23 +0000

    notes

---
+++ Unit 2/bulitinFunctions.py
@@ -0,0 +1,37 @@
+# Function - simply put ; a code block of instructions for computers to follow 
+
+#Bulit - in function - a code block of instruction for a computer to follow that was already written for us.
+# pre - written code instructions
+
+print('hello')
+
+year = input("what year is it")
+print(year)
+
+
+# input() a built in function that allows a user to type in data into the terminal. the data that is passed in will always be treated as a string data type
+
+# print() a built in function that allows a user to print data out in the terminal
+
+# Data casting functions these are pre written fuctions that changes data types from one form to another 
+
+# str() - this datacasting function allows you to change any data tyoe into a stirng 
+
+year = 1944
+
+#print("this event took place in"+ str(year) )
+
+#month = input ("what numerical mouth were you born")
+#print("you were born in the" + month + month of the year)
+
+# string concatennation - combining string data types with one another. We use the plus sign operators tocombine strings together
+
+print(3+3) # adding two intnegers 
+print("3"+"3") # combining 2 strings 
+
+price = 134.99
+print(int(price))
+
+#int() - any data type passed into the brackets wil be covered into a interger(whole number)
+
+# transform a decimal into a whole number cannot transform string into numbers
